cf67a6bd9c9813cbefd475ee96c145916eb2e407
Victor Zeng <Victor.Zxy@outlook.com>
2016-04-29 15:20:53 +0800

    submit

---
+++ src/test/java/org/darkphoenixs/mq/ReceiverTest.java
@@ -0,0 +1,47 @@
+package org.darkphoenixs.mq;
+
+import org.darkphoenixs.kafka.codec.MessageDecoderImpl;
+import org.darkphoenixs.kafka.consumer.MessageConsumer;
+import org.darkphoenixs.kafka.core.KafkaDestination;
+import org.darkphoenixs.kafka.core.KafkaMessageAdapter;
+import org.darkphoenixs.kafka.listener.MessageConsumerListener;
+import org.darkphoenixs.kafka.pool.KafkaMessageReceiverPool;
+import org.darkphoenixs.mq.codec.MessageDecoder;
+import org.darkphoenixs.mq.message.MessageBeanImpl;
+import org.junit.Test;
+import org.springframework.core.io.DefaultResourceLoader;
+
+public class ReceiverTest {
+
+	@Test
+	public void test() throws Exception {
+
+		MessageDecoder<MessageBeanImpl> messageDecoder = new MessageDecoderImpl();
+		
+		KafkaDestination kafkaDestination = new KafkaDestination("QUEUE.TEST");
+		
+		MessageConsumer<MessageBeanImpl> MessageConsumer = new MessageConsumer<MessageBeanImpl>();
+		
+		MessageConsumerListener<MessageBeanImpl> messageConsumerListener = new MessageConsumerListener<MessageBeanImpl>();
+		
+		messageConsumerListener.setConsumer(MessageConsumer);
+		
+		KafkaMessageAdapter<MessageBeanImpl> messageAdapter= new KafkaMessageAdapter<MessageBeanImpl>();
+		
+		messageAdapter.setDecoder(messageDecoder);
+		
+		messageAdapter.setDestination(kafkaDestination);
+		
+		messageAdapter.setMessageListener(messageConsumerListener);
+				
+		KafkaMessageReceiverPool<byte[], byte[]> pool = new KafkaMessageReceiverPool<byte[], byte[]>();
+		
+		pool.setMessageAdapter(messageAdapter);
+		
+		pool.setConfig(new DefaultResourceLoader().getResource("kafka/consumer.properties"));
+		
+		pool.setPoolSize(10);
+		
+//		pool.init();
+	}
+}
---
+++ src/test/java/org/darkphoenixs/mq/SenderTest.java
@@ -0,0 +1,41 @@
+package org.darkphoenixs.mq;
+
+import org.darkphoenixs.kafka.codec.MessageEncoderImpl;
+import org.darkphoenixs.kafka.core.KafkaDestination;
+import org.darkphoenixs.kafka.core.KafkaMessageTemplate;
+import org.darkphoenixs.kafka.pool.KafkaMessageSenderPool;
+import org.darkphoenixs.kafka.producer.MessageProducer;
+import org.darkphoenixs.mq.codec.MessageEncoder;
+import org.darkphoenixs.mq.message.MessageBeanImpl;
+import org.junit.Test;
+import org.springframework.core.io.DefaultResourceLoader;
+
+public class SenderTest {
+
+	@Test
+	public void test() throws Exception {
+		
+		MessageEncoder<MessageBeanImpl>  messageEncoder = new MessageEncoderImpl();
+		
+		KafkaDestination kafkaDestination = new KafkaDestination("QUEUE.TEST");
+
+		KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
+		
+		pool.setConfig(new DefaultResourceLoader().getResource("kafka/producer.properties"));
+		
+		pool.setPoolSize(10);
+		
+		KafkaMessageTemplate<MessageBeanImpl> messageTemplate = new KafkaMessageTemplate<MessageBeanImpl>();
+		
+		messageTemplate.setEncoder(messageEncoder);
+		
+		messageTemplate.setMessageSenderPool(pool);
+		
+		MessageProducer<MessageBeanImpl> messageProducer = new MessageProducer<MessageBeanImpl>();
+		
+		messageProducer.setMessageTemplate(messageTemplate);
+		
+		messageProducer.setDestination(kafkaDestination);
+		
+	}
+}
