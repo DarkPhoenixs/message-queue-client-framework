46916019b173f22440b0ac7bdb7b1dc2894cf8f3
业余布道师 <Victor.Zxy@outlook.com>
2016-04-30 13:51:45 +0800

    add testcase

---
+++ src/test/java/org/darkphoenixs/mq/common/MessageConsumerFactoryTest.java
@@ -0,0 +1,49 @@
+package org.darkphoenixs.mq.common;
+
+import org.darkphoenixs.mq.consumer.AbstractConsumer;
+import org.darkphoenixs.mq.consumer.Consumer;
+import org.darkphoenixs.mq.exception.MQException;
+import org.darkphoenixs.mq.message.MessageBeanImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MessageConsumerFactoryTest {
+
+	@Test
+	public void test_0() throws Exception {
+
+		MessageConsumerFactory factory = (MessageConsumerFactory) MessageConsumerFactory
+				.getInstance();
+
+		ConsumerTest consumer1 = new ConsumerTest();
+		consumer1.setTabName("TabName1");
+		consumer1.setFuncName("FuncName1");
+		consumer1.setIframeName("IframeName1");
+		consumer1.setProtocolId("ProtocolId1");
+
+		ConsumerTest consumer2 = new ConsumerTest();
+		consumer2.setTabName("TabName2");
+		consumer2.setFuncName("FuncName2");
+		consumer2.setIframeName("IframeName2");
+		consumer2.setProtocolId("ProtocolId2");
+		
+		factory.setConsumers(new Consumer[] { consumer1 });
+		factory.addConsumer(consumer2);
+		factory.init();
+		
+		Assert.assertEquals(consumer1, factory.getConsumer("ProtocolId1"));
+		Assert.assertEquals(consumer2, factory.getConsumer("ProtocolId2"));
+		
+		factory.destroy();
+
+	}
+
+	private class ConsumerTest extends AbstractConsumer<MessageBeanImpl> {
+
+		@Override
+		protected void doReceive(MessageBeanImpl message) throws MQException {
+			
+			System.out.println(message);
+		}
+	}
+}
---
+++ src/test/java/org/darkphoenixs/mq/common/MessageProducerFactoryTest.java
@@ -0,0 +1,53 @@
+package org.darkphoenixs.mq.common;
+
+import org.darkphoenixs.mq.exception.MQException;
+import org.darkphoenixs.mq.message.MessageBeanImpl;
+import org.darkphoenixs.mq.producer.Producer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MessageProducerFactoryTest {
+
+	@Test
+	public void test_0() throws Exception {
+
+		MessageProducerFactory factory = (MessageProducerFactory) MessageProducerFactory
+				.getInstance();
+
+		ProducerTest producer1 = new ProducerTest();
+		producer1.setProducerKey("ProducerKey1");
+
+		ProducerTest producer2 = new ProducerTest();
+		producer2.setProducerKey("ProducerKey2");
+
+		factory.setProducers(new Producer[] { producer1 });
+		factory.addProducer(producer2);
+		
+		factory.init();
+		
+		Assert.assertEquals(producer1, factory.getProducer("ProducerKey1"));
+		Assert.assertEquals(producer2, factory.getProducer("ProducerKey2"));
+		
+		factory.destroy();
+	}
+
+	private class ProducerTest implements Producer<MessageBeanImpl> {
+
+		private String producerKey;
+
+		public void setProducerKey(String producerKey) {
+			this.producerKey = producerKey;
+		}
+
+		@Override
+		public String getProducerKey() throws MQException {
+
+			return this.producerKey;
+		}
+
+		@Override
+		public void send(MessageBeanImpl message) throws MQException {
+			System.out.println(message);
+		}
+	}
+}
