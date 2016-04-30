420e5b04c445e5253a9b3c887a86da8faa24e134
业余布道师 <Victor.Zxy@outlook.com>
2016-04-30 23:48:53 +0800

    fix

--- src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageReceiverPoolTest.java
+++ src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageReceiverPoolTest.java
@@ -1,5 +1,7 @@
 package org.darkphoenixs.kafka.pool;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 import kafka.serializer.DefaultDecoder;
@@ -19,73 +21,91 @@ public class KafkaMessageReceiverPoolTest {
 
 	@Test
 	public void test() throws Exception {
-		
+
 		MessageDecoder<MessageBeanImpl> messageDecoder = new MessageDecoderImpl();
-		
+
 		KafkaDestination kafkaDestination = new KafkaDestination("QUEUE.TEST");
-		
+
 		MessageConsumer<MessageBeanImpl> MessageConsumer = new MessageConsumer<MessageBeanImpl>();
-		
+
 		MessageConsumerListener<MessageBeanImpl> messageConsumerListener = new MessageConsumerListener<MessageBeanImpl>();
-		
+
 		messageConsumerListener.setConsumer(MessageConsumer);
-		
-		KafkaMessageAdapter<MessageBeanImpl> messageAdapter= new KafkaMessageAdapter<MessageBeanImpl>();
-		
+
+		KafkaMessageAdapter<MessageBeanImpl> messageAdapter = new KafkaMessageAdapter<MessageBeanImpl>();
+
 		messageAdapter.setDecoder(messageDecoder);
-		
+
 		messageAdapter.setDestination(kafkaDestination);
-		
+
 		messageAdapter.setMessageListener(messageConsumerListener);
-		
+
 		final KafkaMessageReceiverPool<byte[], byte[]> pool = new KafkaMessageReceiverPool<byte[], byte[]>();
-		
+
 		Assert.assertNotNull(pool.getThreadFactory());
 		pool.setThreadFactory(new KafkaPoolThreadFactory());
-		
+
 		Assert.assertNotNull(pool.getProps());
 		pool.setProps(new Properties());
-		
+
 		Assert.assertEquals(0, pool.getPoolSize());
 		pool.setPoolSize(10);
-		
+
 		Assert.assertNull(pool.getZookeeperStr());
 		pool.setZookeeperStr("");
-		
+
 		Assert.assertNull(pool.getClientId());
 		pool.setClientId("test");
-		
+
 		Assert.assertTrue(pool.getAutoCommit());
 		pool.setAutoCommit(false);
-		
+
 		Assert.assertNull(pool.getConfig());
-		pool.setConfig(new DefaultResourceLoader().getResource("kafka/consumer1.properties"));
-		pool.setConfig(new DefaultResourceLoader().getResource("kafka/consumer.properties"));
+		pool.setConfig(new DefaultResourceLoader()
+				.getResource("kafka/consumer1.properties"));
+		pool.setConfig(new DefaultResourceLoader()
+				.getResource("kafka/consumer.properties"));
 
 		Assert.assertSame(DefaultDecoder.class, pool.getKeyDecoderClass());
 		pool.setKeyDecoderClass(DefaultDecoder.class);
-		
+
 		Assert.assertSame(DefaultDecoder.class, pool.getValDecoderClass());
 		pool.setValDecoderClass(DefaultDecoder.class);
-		
+
 		Assert.assertNull(pool.getMessageAdapter());
 		pool.setMessageAdapter(messageAdapter);
-		
+
 		Assert.assertNotNull(pool.getReceiver());
-		
-		Thread thread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				pool.init();
-			}
-		});
-		thread.setDaemon(true);
-		
-		thread.start();
-		
-		Thread.sleep(5000);
-		
-		pool.destroy();
+
+		ping02("10.0.63.11");
+
+		// Thread thread = new Thread(new Runnable() {
+		//
+		// @Override
+		// public void run() {
+		// pool.init();
+		// }
+		// });
+		// thread.setDaemon(true);
+		//
+		// thread.start();
+		//
+		// Thread.sleep(5000);
+		//
+		// pool.destroy();
+	}
+
+	public static void ping02(String ipAddress) throws Exception {
+		String line = null;
+		try {
+			Process pro = Runtime.getRuntime().exec("ping " + ipAddress);
+			BufferedReader buf = new BufferedReader(new InputStreamReader(
+					pro.getInputStream()));
+
+			line = buf.readLine();
+			System.out.println(line);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
