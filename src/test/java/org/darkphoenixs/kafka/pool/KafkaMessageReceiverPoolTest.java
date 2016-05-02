813025dcc0aaacf82c8fc8290d0db9d08cc9a161
业余布道师 <Victor.Zxy@outlook.com>
2016-05-02 21:47:45 +0800

    test

--- src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageReceiverPoolTest.java
+++ src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageReceiverPoolTest.java
@@ -1,43 +1,81 @@
 package org.darkphoenixs.kafka.pool;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.LinkedBlockingQueue;
 
-import kafka.consumer.FetchedDataChunk;
-import kafka.consumer.KafkaStream;
+import kafka.admin.TopicCommand;
 import kafka.serializer.DefaultDecoder;
-import kafka.utils.VerifiableProperties;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.MockTime;
+import kafka.utils.TestUtils;
+import kafka.utils.TestZKUtils;
+import kafka.utils.Time;
+import kafka.utils.ZKStringSerializer$;
+import kafka.zk.EmbeddedZookeeper;
 
+import org.I0Itec.zkclient.ZkClient;
 import org.darkphoenixs.kafka.codec.MessageDecoderImpl;
 import org.darkphoenixs.kafka.consumer.MessageConsumer;
-import org.darkphoenixs.kafka.core.KafkaConstants;
 import org.darkphoenixs.kafka.core.KafkaDestination;
 import org.darkphoenixs.kafka.core.KafkaMessageAdapter;
 import org.darkphoenixs.kafka.listener.MessageConsumerListener;
 import org.darkphoenixs.mq.codec.MessageDecoder;
 import org.darkphoenixs.mq.message.MessageBeanImpl;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class KafkaMessageReceiverPoolTest {
 
-	static Properties props = new Properties();
+	private int brokerId = 0;
+	private String topic = "QUEUE.TEST";
+	private String zkConnect;
+	private EmbeddedZookeeper zkServer;
+	private ZkClient zkClient;
+	private KafkaServer kafkaServer;
+	private int port;
+	private Properties kafkaProps;
 
-	static {
-		Resource resource = new DefaultResourceLoader()
-				.getResource("kafka/consumer.properties");
+	@Before
+	public void before() {
+		zkConnect = TestZKUtils.zookeeperConnect();
+		zkServer = new EmbeddedZookeeper(zkConnect);
+		zkClient = new ZkClient(zkServer.connectString(), 30000, 30000,
+				ZKStringSerializer$.MODULE$);
 
-		try {
-			PropertiesLoaderUtils.fillProperties(props, resource);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// setup Broker
+		port = TestUtils.choosePort();
+		kafkaProps = TestUtils.createBrokerConfig(brokerId, port, true);
+
+		KafkaConfig config = new KafkaConfig(kafkaProps);
+		Time mock = new MockTime();
+		kafkaServer = TestUtils.createServer(config, mock);
+
+		// create topic
+		TopicCommand.TopicCommandOptions options = new TopicCommand.TopicCommandOptions(
+				new String[] { "--create", "--topic", topic,
+						"--replication-factor", "1", "--partitions", "1" });
+
+		TopicCommand.createTopic(zkClient, options);
+
+		List<KafkaServer> servers = new ArrayList<KafkaServer>();
+		servers.add(kafkaServer);
+		TestUtils.waitUntilMetadataIsPropagated(
+				scala.collection.JavaConversions.asScalaBuffer(servers), topic,
+				0, 5000);
 	}
-	
+
+	@After
+	public void after() {
+		kafkaServer.shutdown();
+		zkClient.close();
+		zkServer.shutdown();
+	}
+
 	@Test
 	public void test() throws Exception {
 
@@ -67,6 +105,9 @@ public class KafkaMessageReceiverPoolTest {
 		pool.setConfig(new DefaultResourceLoader()
 				.getResource("kafka/consumer.properties"));
 
+		pool.setProps(TestUtils.createConsumerProperties(
+				zkServer.connectString(), "group_1", "consumer_id", 1000));
+
 		Assert.assertSame(DefaultDecoder.class, pool.getKeyDecoderClass());
 		pool.setKeyDecoderClass(DefaultDecoder.class);
 
@@ -78,140 +119,23 @@ public class KafkaMessageReceiverPoolTest {
 
 		Assert.assertNotNull(pool.getReceiver());
 
-		Thread thread1 = new Thread(new Runnable() {
+		pool.getBrokerStr(topic);
 
-			@Override
-			public void run() {
-				Thread thread1 = new Thread(new Runnable() {
+		pool.getPartitionNum(topic);
 
-					@Override
-					public void run() {
-						pool.getBrokerStr("QUEUE.TEST");
-					}
-				});
+		pool.init();
 
-				thread1.setDaemon(true);
-
-				thread1.start();
-			}
-		});
-
-		thread1.setDaemon(true);
-
-		thread1.start();
-
-		Thread thread2 = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				Thread thread2 = new Thread(new Runnable() {
-
-					@Override
-					public void run() {
-						pool.getPartitionNum("QUEUE.TEST");
-					}
-				});
-
-				thread2.setDaemon(true);
-
-				thread2.start();
-			}
-		});
-
-		thread2.setDaemon(true);
-
-		thread2.start();
-
-		Thread thread3 = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				pool.init();
-			}
-		});
-
-		thread3.setDaemon(true);
-
-		thread3.start();
-
+		Thread.sleep(5000);
+		
 		pool.destroy();
 
-	}
-
-	@Test
-	public void test1() throws Exception {
-
-		KafkaMessageReceiverPool<byte[], byte[]> pool = new KafkaMessageReceiverPool<byte[], byte[]>();
-
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/consumer.properties"));
-		pool.setMessageAdapter(getAdapter());
-		pool.setAutoCommit(false);
-
-		KafkaStream<byte[], byte[]> stream = new KafkaStream<byte[], byte[]>(
-				new LinkedBlockingQueue<FetchedDataChunk>(), 1000,
-				new DefaultDecoder(new VerifiableProperties(pool.getProps())),
-				new DefaultDecoder(new VerifiableProperties(pool.getProps())),
-				"test");
-
-		KafkaMessageReceiverPool<byte[], byte[]>.ReceiverThread thread = pool.new ReceiverThread(
-				stream, pool.getMessageAdapter(), 1);
-
-		Thread thread4 = new Thread(thread);
-
-		// thread4.setDaemon(true);
-
-		thread4.start();
-
-	}
-
-	@Test
-	public void test2() throws Exception {
-
-		KafkaMessageReceiverPool<byte[], byte[]> pool = new KafkaMessageReceiverPool<byte[], byte[]>();
-
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/consumer.properties"));
-		pool.setMessageAdapter(getAdapter());
-		pool.setAutoCommit(false);
-
-		KafkaMessageReceiverPool<byte[], byte[]>.ReceiverThread thread = pool.new ReceiverThread(
-				null, pool.getMessageAdapter(), 1);
-
-		Thread thread4 = new Thread(thread);
-
-		// thread4.setDaemon(true);
-
-		thread4.start();
-
-	}
-
-	@Test
-	public void test3() throws Exception {
-
-		KafkaMessageReceiverPool<byte[], byte[]> pool = new KafkaMessageReceiverPoolImpl();
-
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/consumer.properties"));
-		pool.setMessageAdapter(getAdapter());
-		pool.setAutoCommit(false);
-		
-		try {
-			pool.init();
-		} catch (Exception e) {
-			Assert.assertNotNull(e);
-		}
-		
-		Assert.assertNotNull(pool.getReceiver());
-
-		pool.destroy();
 	}
 
 	private KafkaMessageAdapter<MessageBeanImpl> getAdapter() {
 
 		MessageDecoder<MessageBeanImpl> messageDecoder = new MessageDecoderImpl();
 
-		KafkaDestination kafkaDestination = new KafkaDestination("QUEUE.TEST");
+		KafkaDestination kafkaDestination = new KafkaDestination(topic);
 
 		MessageConsumer<MessageBeanImpl> MessageConsumer = new MessageConsumer<MessageBeanImpl>();
 
@@ -230,19 +154,4 @@ public class KafkaMessageReceiverPoolTest {
 		return messageAdapter;
 	}
 
-	private class KafkaMessageReceiverPoolImpl extends
-			KafkaMessageReceiverPool<byte[], byte[]> {
-
-		@Override
-		public String getBrokerStr(String topic) {
-
-			return props.getProperty(KafkaConstants.ZOOKEEPER_LIST);
-		}
-
-		@Override
-		public int getPartitionNum(String topic) {
-
-			return 1;
-		}
-	}
 }
