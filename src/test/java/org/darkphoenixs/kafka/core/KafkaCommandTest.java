8714d496067287ed51c9c005c92af07ee0dffb44
业余布道师 <Victor.Zxy@outlook.com>
2016-05-02 22:34:55 +0800

    add testcase

--- src/test/java/org/darkphoenixs/kafka/core/KafkaCommandTest.java
+++ src/test/java/org/darkphoenixs/kafka/core/KafkaCommandTest.java
@@ -1,216 +1,92 @@
 package org.darkphoenixs.kafka.core;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
+import kafka.admin.TopicCommand;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.MockTime;
+import kafka.utils.TestUtils;
+import kafka.utils.TestZKUtils;
+import kafka.utils.Time;
+import kafka.utils.ZKStringSerializer$;
+import kafka.zk.EmbeddedZookeeper;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class KafkaCommandTest {
 
-	static Properties props = new Properties();
+	private int brokerId = 0;
+	private String topic = "QUEUE.TEST";
+	private String zkConnect;
+	private EmbeddedZookeeper zkServer;
+	private ZkClient zkClient;
+	private KafkaServer kafkaServer;
+	private int port;
+	private Properties kafkaProps;
 
-	static {
-		Resource resource = new DefaultResourceLoader()
-				.getResource("kafka/consumer.properties");
+	@Before
+	public void before() {
+		zkConnect = TestZKUtils.zookeeperConnect();
+		zkServer = new EmbeddedZookeeper(zkConnect);
+		zkClient = new ZkClient(zkServer.connectString(), 30000, 30000,
+				ZKStringSerializer$.MODULE$);
 
-		try {
-			PropertiesLoaderUtils.fillProperties(props, resource);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// setup Broker
+		port = TestUtils.choosePort();
+		kafkaProps = TestUtils.createBrokerConfig(brokerId, port, true);
+
+		KafkaConfig config = new KafkaConfig(kafkaProps);
+		Time mock = new MockTime();
+		kafkaServer = TestUtils.createServer(config, mock);
+
+		// create topic
+		TopicCommand.TopicCommandOptions options = new TopicCommand.TopicCommandOptions(
+				new String[] { "--create", "--topic", topic,
+						"--replication-factor", "1", "--partitions", "1" });
+
+		TopicCommand.createTopic(zkClient, options);
+
+		List<KafkaServer> servers = new ArrayList<KafkaServer>();
+		servers.add(kafkaServer);
+		TestUtils.waitUntilMetadataIsPropagated(
+				scala.collection.JavaConversions.asScalaBuffer(servers), topic,
+				0, 5000);
+	}
+
+	@After
+	public void after() {
+		kafkaServer.shutdown();
+		zkClient.close();
+		zkServer.shutdown();
 	}
 
 	@Test
-	public void test1() throws Exception {
+	public void test() throws Exception {
 
 		Assert.assertNotNull(new KafkaCommand());
 
-		Thread thread = new Thread(new Runnable() {
+		KafkaCommand.createOptions(zkServer.connectString(), "COMMAND.TEST", 1, 1);
 
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.createOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST", 1, 1);
-				} catch (Exception e) {
+		KafkaCommand.listOptions(zkServer.connectString());
 
-					Assert.assertNotNull(e);
-				}
-			}
-		});
+		KafkaCommand.selectOptions(zkServer.connectString(), "COMMAND.TEST");
 
-		thread.setDaemon(true);
+		KafkaCommand.updateOptions(zkServer.connectString(), "COMMAND.TEST", 2);
 
-		thread.start();
+		KafkaCommand.updateOptions(zkServer.connectString(), "COMMAND.TEST", "flush.messages=1", "max.message.bytes");
 
+		KafkaCommand.updateOptions(zkServer.connectString(), "COMMAND.TEST", 3, "flush.messages", "max.message.bytes=64000");
+
+		KafkaCommand.deleteOptions(zkServer.connectString(), "COMMAND.TEST");
+
+		KafkaCommand.topicCommand("--list", "--zookeeper", zkServer.connectString());
 	}
 
-	@Test
-	public void test2() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.listOptions(props
-							.getProperty(KafkaConstants.ZOOKEEPER_LIST));
-
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-
-	}
-
-	@Test
-	public void test3() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.selectOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST");
-
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-
-	}
-
-	@Test
-	public void test4() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.updateOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST", 2);
-
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-	}
-
-	@Test
-	public void test5() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.updateOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST", "1=1", "2");
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-	}
-
-	@Test
-	public void test6() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.updateOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST", 1, "1=1", "2");
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-	}
-
-	@Test
-	public void test7() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.topicCommand("--list --zookeeper "
-							+ props.getProperty(KafkaConstants.ZOOKEEPER_LIST));
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-	}
-
-	@Test
-	public void test8() throws Exception {
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					KafkaCommand.deleteOptions(
-							props.getProperty(KafkaConstants.ZOOKEEPER_LIST),
-							"QUEUE.TEST");
-				} catch (Exception e) {
-
-					Assert.assertNotNull(e);
-				}
-			}
-		});
-
-		thread.setDaemon(true);
-
-		thread.start();
-	}
 }
