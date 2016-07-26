fb99b6afeb2f47a4e31aa12fb5fb851a8fdaec69
Victor Zeng <Victor.Zxy@outlook.com>
2016-07-26 15:38:56 +0800

    add new kafka producer

---
+++ src/main/java/org/darkphoenixs/kafka/core/KafkaMessageNewSender.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2015-2016 Dark Phoenixs (Open-Source Organization).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.darkphoenixs.kafka.core;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.PartitionInfo;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * <p>Title: KafkaMessageNewSender</p>
+ * <p>Description: Kafka消息新发送器</p>
+ *
+ * @param <K> the type of message key
+ * @param <V> the type of message value
+ * @author Victor.Zxy
+ * @version 1.4.0
+ * @see KafkaMessageSender
+ * @since 2016 /7/25.
+ */
+public class KafkaMessageNewSender<K, V> implements KafkaMessageSender<K, V> {
+
+    /**
+     * Atomic singleton instance.
+     */
+    private static final AtomicReference<KafkaMessageNewSender> instance = new AtomicReference<>();
+
+    /**
+     * The producer is thread safe and sharing a single producer instance
+     * across threads will generally be faster than having multiple instances.
+     */
+    private final KafkaProducer<K, V> kafkaProducer;
+
+    /**
+     * @param properties the properties
+     */
+    private KafkaMessageNewSender(Properties properties) {
+
+        kafkaProducer = new KafkaProducer<>(properties);
+    }
+
+    /**
+     * Gets or Create instance.
+     *
+     * @param properties the properties
+     * @return the or create instance
+     */
+    public static KafkaMessageNewSender getOrCreateInstance(Properties properties) {
+
+        if (instance.get() == null)
+
+            instance.set(new KafkaMessageNewSender(properties));
+
+        return instance.get();
+    }
+
+    /**
+     * Gets topic partitions.
+     *
+     * @param topic the topic
+     * @return the partitions
+     */
+    public List<PartitionInfo> getPartitions(String topic) {
+
+        return kafkaProducer.partitionsFor(topic);
+    }
+
+    @Override
+    public void send(String topic, V value) {
+
+        kafkaProducer.send(new ProducerRecord<K, V>(topic, value));
+    }
+
+    @Override
+    public void sendWithKey(String topic, K key, V value) {
+
+        kafkaProducer.send(new ProducerRecord<>(topic, key, value));
+    }
+
+    @Override
+    public void close() {
+
+        // flush the producer
+        kafkaProducer.flush();
+    }
+
+    @Override
+    public void shutDown() {
+
+        kafkaProducer.close();
+    }
+}
---
+++ src/test/java/org/darkphoenixs/kafka/core/KafkaMessageNewSenderTest.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2016. Dark Phoenixs (Open-Source Organization).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.darkphoenixs.kafka.core;
+
+import kafka.admin.TopicCommand;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.MockTime;
+import kafka.utils.TestUtils;
+import kafka.utils.Time;
+import kafka.utils.ZkUtils;
+import kafka.zk.EmbeddedZookeeper;
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.kafka.common.protocol.SecurityProtocol;
+import org.apache.kafka.common.security.JaasUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import scala.Option;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by Victor.Zxy on 2016/7/26.
+ */
+public class KafkaMessageNewSenderTest {
+
+    private int brokerId = 0;
+    private String topic = "QUEUE.TEST";
+    private String zkConnect;
+    private EmbeddedZookeeper zkServer;
+    private ZkClient zkClient;
+    private KafkaServer kafkaServer;
+    private int port = 9999;
+    private Properties kafkaProps;
+
+    @Before
+    public void setUp() throws Exception {
+
+        zkServer = new EmbeddedZookeeper();
+        zkConnect = String.format("localhost:%d", zkServer.port());
+        ZkUtils zkUtils = ZkUtils.apply(zkConnect, 30000, 30000,
+                JaasUtils.isZkSecurityEnabled());
+        zkClient = zkUtils.zkClient();
+
+        final Option<File> noFile = scala.Option.apply(null);
+        final Option<SecurityProtocol> noInterBrokerSecurityProtocol = scala.Option
+                .apply(null);
+
+        kafkaProps = TestUtils.createBrokerConfig(brokerId, zkConnect, false,
+                false, port, noInterBrokerSecurityProtocol, noFile, true,
+                false, TestUtils.RandomPort(), false, TestUtils.RandomPort(),
+                false, TestUtils.RandomPort());
+
+        kafkaProps.setProperty("auto.create.topics.enable", "true");
+        kafkaProps.setProperty("num.partitions", "1");
+        // We *must* override this to use the port we allocated (Kafka currently
+        // allocates one port
+        // that it always uses for ZK
+        kafkaProps.setProperty("zookeeper.connect", this.zkConnect);
+
+        KafkaConfig config = new KafkaConfig(kafkaProps);
+        Time mock = new MockTime();
+        kafkaServer = TestUtils.createServer(config, mock);
+
+        // create topic
+        TopicCommand.TopicCommandOptions options = new TopicCommand.TopicCommandOptions(
+                new String[]{"--create", "--topic", topic,
+                        "--replication-factor", "1", "--partitions", "4"});
+
+        TopicCommand.createTopic(zkUtils, options);
+
+        List<KafkaServer> servers = new ArrayList<KafkaServer>();
+        servers.add(kafkaServer);
+        TestUtils.waitUntilMetadataIsPropagated(
+                scala.collection.JavaConversions.asScalaBuffer(servers), topic,
+                0, 5000);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        kafkaServer.shutdown();
+        zkClient.close();
+        zkServer.shutdown();
+    }
+
+    @Test
+    public void getOrCreatInstance() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        KafkaMessageNewSender<byte[], byte[]> sender2 = KafkaMessageNewSender.getOrCreateInstance(null);
+
+        Assert.assertEquals(sender, sender2);
+
+        Assert.assertSame(sender, sender2);
+
+        sender.shutDown();
+    }
+
+    @Test
+    public void getPartitions() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        Assert.assertEquals(sender.getPartitions(topic).size(), 4);
+
+        sender.shutDown();
+    }
+
+    @Test
+    public void send() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        sender.send(topic, "hahah".getBytes());
+
+        sender.shutDown();
+    }
+
+    @Test
+    public void sendWithKey() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        sender.sendWithKey(topic, "key".getBytes(), "value".getBytes());
+
+        sender.shutDown();
+    }
+
+    @Test
+    public void close() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        sender.close();
+
+        sender.shutDown();
+    }
+
+    @Test
+    public void shutDown() throws Exception {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:" + port);
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        KafkaMessageNewSender<byte[], byte[]> sender = KafkaMessageNewSender.getOrCreateInstance(properties);
+
+        sender.shutDown();
+    }
+
+}
