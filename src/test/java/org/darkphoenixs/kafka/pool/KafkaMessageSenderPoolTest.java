d51f45822743c9ae9ab21648e3b8f4f59051bd97
业余布道师 <Victor.Zxy@outlook.com>
2016-08-08 21:04:01 +0800

    fix issue.

--- src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageSenderPoolTest.java
+++ src/test/java/org/darkphoenixs/kafka/pool/KafkaMessageSenderPoolTest.java
@@ -11,6 +11,7 @@ import kafka.zk.EmbeddedZookeeper;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.kafka.common.protocol.SecurityProtocol;
 import org.apache.kafka.common.security.JaasUtils;
+import org.darkphoenixs.kafka.core.KafkaConstants;
 import org.darkphoenixs.kafka.core.KafkaMessageSender;
 import org.darkphoenixs.kafka.core.ZookeeperHosts;
 import org.junit.After;
@@ -23,170 +24,196 @@ import scala.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.*;
 
 public class KafkaMessageSenderPoolTest {
 
-	private int brokerId = 0;
-	private String topic = "QUEUE.TEST";
-	private String zkConnect;
-	private EmbeddedZookeeper zkServer;
-	private ZkClient zkClient;
-	private KafkaServer kafkaServer;
-	private int port = 9999;
-	private Properties kafkaProps;
+    private int brokerId = 0;
+    private String topic = "QUEUE.TEST";
+    private String zkConnect;
+    private EmbeddedZookeeper zkServer;
+    private ZkClient zkClient;
+    private KafkaServer kafkaServer;
+    private int port = 9999;
+    private Properties kafkaProps;
 
-	@Before
-	public void before() {
+    @Before
+    public void before() {
 
-		zkServer = new EmbeddedZookeeper();
-		zkConnect = String.format("localhost:%d", zkServer.port());
-		ZkUtils zkUtils = ZkUtils.apply(zkConnect, 30000, 30000,
-				JaasUtils.isZkSecurityEnabled());
-		zkClient = zkUtils.zkClient();
+        zkServer = new EmbeddedZookeeper();
+        zkConnect = String.format("localhost:%d", zkServer.port());
+        ZkUtils zkUtils = ZkUtils.apply(zkConnect, 30000, 30000,
+                JaasUtils.isZkSecurityEnabled());
+        zkClient = zkUtils.zkClient();
 
-		final Option<java.io.File> noFile = scala.Option.apply(null);
-		final Option<SecurityProtocol> noInterBrokerSecurityProtocol = scala.Option
-				.apply(null);
+        final Option<java.io.File> noFile = scala.Option.apply(null);
+        final Option<SecurityProtocol> noInterBrokerSecurityProtocol = scala.Option
+                .apply(null);
 
-		kafkaProps = TestUtils.createBrokerConfig(brokerId, zkConnect, false,
-				false, port, noInterBrokerSecurityProtocol, noFile, true,
-				false, TestUtils.RandomPort(), false, TestUtils.RandomPort(),
-				false, TestUtils.RandomPort());
+        kafkaProps = TestUtils.createBrokerConfig(brokerId, zkConnect, false,
+                false, port, noInterBrokerSecurityProtocol, noFile, true,
+                false, TestUtils.RandomPort(), false, TestUtils.RandomPort(),
+                false, TestUtils.RandomPort());
 
-		kafkaProps.setProperty("auto.create.topics.enable", "true");
-		kafkaProps.setProperty("num.partitions", "1");
-		// We *must* override this to use the port we allocated (Kafka currently
-		// allocates one port
-		// that it always uses for ZK
-		kafkaProps.setProperty("zookeeper.connect", this.zkConnect);
-		kafkaProps.setProperty("host.name", "localhost");
-		kafkaProps.setProperty("port", port + "");
+        kafkaProps.setProperty("auto.create.topics.enable", "true");
+        kafkaProps.setProperty("num.partitions", "1");
+        // We *must* override this to use the port we allocated (Kafka currently
+        // allocates one port
+        // that it always uses for ZK
+        kafkaProps.setProperty("zookeeper.connect", this.zkConnect);
+        kafkaProps.setProperty("host.name", "localhost");
+        kafkaProps.setProperty("port", port + "");
 
-		KafkaConfig config = new KafkaConfig(kafkaProps);
-		Time mock = new MockTime();
-		kafkaServer = TestUtils.createServer(config, mock);
+        KafkaConfig config = new KafkaConfig(kafkaProps);
+        Time mock = new MockTime();
+        kafkaServer = TestUtils.createServer(config, mock);
 
-		// create topic
-		TopicCommand.TopicCommandOptions options = new TopicCommand.TopicCommandOptions(
-				new String[] { "--create", "--topic", topic,
-						"--replication-factor", "1", "--partitions", "1" });
+        // create topic
+        TopicCommand.TopicCommandOptions options = new TopicCommand.TopicCommandOptions(
+                new String[]{"--create", "--topic", topic,
+                        "--replication-factor", "1", "--partitions", "1"});
 
-		TopicCommand.createTopic(zkUtils, options);
+        TopicCommand.createTopic(zkUtils, options);
 
-		List<KafkaServer> servers = new ArrayList<KafkaServer>();
-		servers.add(kafkaServer);
-		TestUtils.waitUntilMetadataIsPropagated(
-				scala.collection.JavaConversions.asScalaBuffer(servers), topic,
-				0, 5000);
-	}
+        List<KafkaServer> servers = new ArrayList<KafkaServer>();
+        servers.add(kafkaServer);
+        TestUtils.waitUntilMetadataIsPropagated(
+                scala.collection.JavaConversions.asScalaBuffer(servers), topic,
+                0, 5000);
+    }
 
-	@After
-	public void after() {
-		kafkaServer.shutdown();
-		zkClient.close();
-		zkServer.shutdown();
-	}
+    @After
+    public void after() {
+        kafkaServer.shutdown();
+        zkClient.close();
+        zkServer.shutdown();
+    }
 
-	@Test
-	public void test_0() throws Exception {
+    @Test
+    public void test_0() throws Exception {
 
-		KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
+        KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
 
-		Assert.assertNull(pool.getThreadFactory());
-		pool.setThreadFactory(new KafkaPoolThreadFactory());
+        Assert.assertNull(pool.getThreadFactory());
+        pool.setThreadFactory(new KafkaPoolThreadFactory());
 
-		Assert.assertNotNull(pool.getProps());
-		pool.setProps(new Properties());
+        Assert.assertNotNull(pool.getProps());
+        pool.setProps(new Properties());
 
-		Assert.assertEquals(0, pool.getPoolSize());
-		pool.setPoolSize(10);
+        Assert.assertEquals(0, pool.getPoolSize());
+        pool.setPoolSize(10);
 
-		Assert.assertNull(pool.getClientId());
-		pool.setClientId("test");
+        Assert.assertNull(pool.getClientId());
+        pool.setClientId("test");
 
-		Assert.assertNull(pool.getBrokerStr());
-		pool.setBrokerStr("");
+        Assert.assertNull(pool.getBrokerStr());
+        pool.setBrokerStr("");
 
-		Assert.assertNull(pool.getConfig());
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/producer1.properties"));
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/producer.properties"));
+        Assert.assertNull(pool.getConfig());
+        pool.setConfig(new DefaultResourceLoader()
+                .getResource("kafka/producer1.properties"));
+        pool.setConfig(new DefaultResourceLoader()
+                .getResource("kafka/producer.properties"));
 
-		pool.setProps(TestUtils.getProducerConfig("localhost:" + port));
+        pool.setProps(TestUtils.getProducerConfig("localhost:" + port));
 
-		pool.setZkhosts(new ZookeeperHosts(zkConnect, topic));
+        pool.setZkhosts(new ZookeeperHosts(zkConnect, topic));
 
-		pool.init();
+        pool.init();
 
-		Assert.assertNotNull(pool.getSender());
+        Assert.assertNotNull(pool.getSender());
 
-		Assert.assertNotNull(pool.getSender());
+        Assert.assertNotNull(pool.getSender());
 
-		KafkaMessageSender<byte[], byte[]> sender = pool.getSender();
+        KafkaMessageSender<byte[], byte[]> sender = pool.getSender();
 
-		pool.returnSender(sender);
-		pool.returnSender(sender);
+        pool.returnSender(sender);
+        pool.returnSender(sender);
 
-		pool.destroy();
-	}
+        pool.destroy();
+    }
 
-	@Test
-	public void test_1() throws Exception {
+    @Test
+    public void test_1() throws Exception {
 
-		final KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
+        final KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
 
-		pool.getSender();
+        pool.getSender();
 
-		Thread thread1 = new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				pool.setPoolSize(20);
-				pool.init();
-			}
-		});
+            @Override
+            public void run() {
+                pool.setPoolSize(20);
+                pool.init();
+            }
+        });
 
-		thread1.start();
+        thread1.start();
 
 
-		Thread thread2 = new Thread(new Runnable() {
+        Thread thread2 = new Thread(new Runnable() {
 
-			@Override
-			public void run() {
+            @Override
+            public void run() {
 
-				pool.setPoolSize(0);
-				pool.init();
-			}
-		});
+                pool.setPoolSize(0);
+                pool.init();
+            }
+        });
 
-		thread2.start();
+        thread2.start();
 
-		pool.destroy();
-	}
+        pool.destroy();
+    }
 
-	@Test
-	public void test_2() throws Exception {
+    @Test
+    public void test_2() throws Exception {
 
-		KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
+        KafkaMessageSenderPool<byte[], byte[]> pool = new KafkaMessageSenderPool<byte[], byte[]>();
 
-		pool.setConfig(new DefaultResourceLoader()
-				.getResource("kafka/producer.properties"));
+        pool.setConfig(new DefaultResourceLoader()
+                .getResource("kafka/producer.properties"));
 
-		pool.setProps(TestUtils.getProducerConfig("localhost:" + port));
+        pool.setProps(TestUtils.getProducerConfig("localhost:" + port));
 
-		pool.setZkhosts(new ZookeeperHosts(zkConnect, topic));
+        pool.setZkhosts(new ZookeeperHosts(zkConnect, topic));
 
-		pool.init();
+        pool.init();
 
-		for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
 
-			try {
-				pool.getSender();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		pool.destroy();
-	}
+            try {
+                pool.getSender();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        pool.destroy();
+
+
+        KafkaMessageSenderPoolDemo poolDemo = new KafkaMessageSenderPoolDemo();
+
+        poolDemo.getSender();
+
+        poolDemo.init();
+
+        poolDemo.getSender();
+
+        poolDemo.destroy();
+    }
+
+
+    class KafkaMessageSenderPoolDemo extends KafkaMessageSenderPool<byte[], byte[]> {
+
+        @Override
+        public synchronized void init() {
+
+            this.freeSender = new Semaphore(1);
+
+            this.queue = new LinkedBlockingQueue<KafkaMessageSender<byte[], byte[]>>(1);
+
+            this.pool = Executors.newFixedThreadPool(1, new KafkaPoolThreadFactory());
+        }
+    }
 }
